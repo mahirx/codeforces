fb884ec88f34097f3d1b4fcc5c1cb7e12ed1b485
mahirx <60250437+mahirx@users.noreply.github.com>
2021-04-19 20:01:47 +0600

    Update b.cpp

--- contest_1509/b.cpp
+++ contest_1509/b.cpp
@@ -49,35 +49,8 @@ int main(int argc, char const *argv[])
 
 
 
-		for (j = 0; j < n; j++)
-		{
-			if (a[j] == 1)
-			{
-				a[j]=2;
-				for (j1 = j+1; j1 < n; j1++)
-				{
-					if (a[j1] == 0)
-					{
-						a[j1]=2;
-						for (j2= j1+1; j2 < n; j2++)
-						{
-							if (a[j2] == 1)
-							{
-								a[j2]=2;
-								sum=sum+1;
-
-								
-							}
-						}
-					
-					}
-				}
-			}
-		}
 
 
-
-		printf("%d\n", sum);
 
 
 
